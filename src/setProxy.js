3a9cc616a1600c9cbbd9774a2d77edb4d5e015a7
ghdwnstlr1188 <ghdwnstlr1188@gmail.com>
2024-11-22 01:08:55 +0900

    서울시 공공데이터 https

--- src/setProxy.js
+++ src/setProxy.js
@@ -17,7 +17,7 @@ export default function (app) {
 
   app.use(
     createProxyMiddleware({
-      target: 'http://openapi.seoul.go.kr:8088',
+      target: 'https://openapi.seoul.go.kr:8088',
       changeOrigin: true
     })
   )
