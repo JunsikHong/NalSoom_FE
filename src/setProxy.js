598f92dcfcb73d0a22adc5574ad54792f7995ef9
ghdwnstlr1188 <ghdwnstlr1188@gmail.com>
2024-11-24 19:52:31 +0900

    api 요청 수정

--- src/setProxy.js
+++ src/setProxy.js
@@ -15,11 +15,10 @@ export default function (app) {
     })
   );
 
-  app.use( '/seoulapi',
+  app.use(
     createProxyMiddleware({
       target: 'http://openapi.seoul.go.kr:8088',
       changeOrigin: true,
-      pathRewrite: { "^/seoulapi" : "" }
     })
   )
 };
